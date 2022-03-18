5fdaf3c501acc26c67516ab2c5da704ae642ed27
lukeperrin10 <lukeperrin27@gmail.com>
2022-03-19 00:37:27 +0100

    WIP styling of form section

--- components/contactUs.jsx
+++ components/contactUs.jsx
@@ -7,58 +7,68 @@ export default function ContactForm() {
       data-netlify-honeypot="bot-field"
       data-cy="contact-us-form"
     >
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>Mitt namn är</label> <br />
+      <div className="contact_us_form_main">
+        <div className="contact-us-one">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Mitt namn är</label> <br />
+            <input
+              type="name"
+              name="name"
+              placeholder="Ditt namn"
+              id="yourname"
+              data-cy="name"
+            />
+          </p>
+          <p>
+            <label>Kontaka mig på </label> <br />
+            <input
+              type="email"
+              name="email"
+              placeholder="Din email"
+              id="youremail"
+              data-cy="email"
+            />
+          </p>
+          <p>
+            <label>Mitt telefonnummer är</label> <br />
+            <input
+              type="tel"
+              name="phone"
+              placeholder="Ditt telefonnummer"
+              id="yourphone"
+              data-cy="phone"
+            />
+          </p>
+        </div>
+        <div className="contact-us-two">
+          <p>
+            <label>Meddelande</label> <br />
+            <textarea
+              name="message"
+              placeholder="Meddelande"
+              id="yourmessage"
+              data-cy="message"
+            />
+          </p>
+        </div>
+      </div>
+      <div className="contact-us-three">
+        <label>Jag accepterar Walborg Events privacy policy</label>
         <input
-          type="name"
-          name="name"
-          placeholder="Ditt namn"
-          id="yourname"
-          data-cy="name"
+          type="checkbox"
+          name="privacy-policy"
+          id="privacy-policy"
+          data-cy="privacy-policy"
+          required="true"
         />
-      </p>
-      <p>
-        <label>Kontaka mig på </label> <br />
-        <input
-          type="email"
-          name="email"
-          placeholder="Din email"
-          id="youremail"
-          data-cy="email"
-        />
-      </p>
-      <p>
-        <label>Mitt telefonnummer är</label> <br />
-        <input
-          type="tel"
-          name="phone"
-          placeholder="Ditt telefonnummer"
-          id="yourphone"
-          data-cy="phone"
-        />
-      </p>
-      <p>
-        <label>Meddelande</label> <br />
-        <textarea
-          name="message"
-          placeholder="Meddelande"
-          id="yourmessage"
-          data-cy="message"
-        />
-      </p>
-      <input
-        type="checkbox"
-        name="privacy-policy"
-        id="privacy-policy"
-        data-cy="privacy-policy"
-      />
-      <label>Jag accepterar Walborg Events privacy policy</label>
-      <p>
-        <button type="submit" data-cy="submit">
-          Skicka
-        </button>
-      </p>
+
+        <div>
+          <button type="submit" data-cy="submit">
+            Skicka
+          </button>
+        </div>
+      </div>
     </form>
   );
 }
