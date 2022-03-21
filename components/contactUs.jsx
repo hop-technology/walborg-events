edc640e916da1f72ea83b585b8e632bcee92ed31
lukeperrin10 <lukeperrin27@gmail.com>
2022-03-21 13:45:34 +0100

    styles button and checkbox area to match design adds sad path tests

--- components/contactUs.jsx
+++ components/contactUs.jsx
@@ -1,82 +1,93 @@
 export default function ContactForm() {
+  const handleError = (e) => {
+    e.target.setCustomValidity(
+      "Du måste godkänna vår policy för att kunna skicka in ditt meddelande"
+    );
+  };
   return (
     <>
-      <div className='contact'>
-        <div className='contact__container'>
-          <h1 data-cy='contact-form-title' className='contact__title'>
+      <div className="contact">
+        <div className="contact__container">
+          <h1 data-cy="contact-form-title" className="contact__title">
             Kontakta oss
           </h1>
           <form
-            className='form'
-            name='contact'
-            method='post'
-            data-netlify='true'
-            data-netlify-honeypot='bot-field'
-            data-cy='contact-us-form'>
-            <div className='form__part1'>
-              <input type='hidden' name='form-name' value='contact' />
-              <div className='form__input'>
+            className="form"
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            data-cy="contact-us-form"
+          >
+            <div className="form__part1">
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="form__input">
                 <label>Mitt namn är</label> <br />
                 <input
-                  type='name'
-                  name='name'
-                  placeholder='Ditt namn'
-                  id='yourname'
-                  data-cy='name'
+                  type="name"
+                  name="name"
+                  placeholder="Ditt namn"
+                  id="yourname"
+                  data-cy="name"
                 />
               </div>
-              <div className='form__input'>
+              <div className="form__input">
                 <label>Kontaka mig på </label> <br />
                 <input
-                  type='email'
-                  name='email'
-                  placeholder='Din email'
-                  id='youremail'
-                  data-cy='email'
+                  type="email"
+                  name="email"
+                  placeholder="Din email"
+                  id="youremail"
+                  data-cy="email"
                 />
               </div>
-              <div className='form__input'>
+              <div className="form__input">
                 <label>Mitt telefonnummer är</label> <br />
                 <input
-                  type='tel'
-                  name='phone'
-                  placeholder='Ditt telefonnummer'
-                  id='yourphone'
-                  data-cy='phone'
+                  type="tel"
+                  name="phone"
+                  placeholder="Ditt telefonnummer"
+                  id="yourphone"
+                  data-cy="phone"
                 />
               </div>
             </div>
-            <div className='form__part2'>
+            <div className="form__part2">
               <p>
                 <label>Meddelande</label> <br />
                 <textarea
-                  name='message'
-                  placeholder='Meddelande'
-                  id='yourmessage'
-                  data-cy='message'
+                  name="message"
+                  placeholder="Meddelande"
+                  id="yourmessage"
+                  data-cy="message"
                 />
               </p>
             </div>
-            <div className='form__part3'>
-              <label>Jag accepterar Walborg Events privacy policy</label>
-              <input
-                type='checkbox'
-                name='privacy-policy'
-                id='privacy-policy'
-                data-cy='privacy-policy'
-                required='true'
-              />
-              <div>
-                <button className='btn__submit' type='submit' data-cy='submit'>
-                  <div className='btn__submit--border'>
-                    <p className='btn__submit--text'>Skicka</p>
-                  </div>
-                </button>
+            <div className="form__part3">
+              <div className="form__part3__checkbox">
+                <input
+                  type="checkbox"
+                  name="privacy-policy"
+                  id="privacy-policy"
+                  data-cy="privacy-policy"
+                  required
+                  onInvalid={handleError}
+                />
               </div>
+              <div>
+                <label>Jag accepterar Walborg Events privacy policy</label>
+              </div>
+            </div>
+            <div className="form__button__box">
+              <button className="btn__submit" type="submit" data-cy="submit">
+                <div className="btn__submit--border">
+                  <p className="btn__submit--text">Skicka</p>
+                </div>
+              </button>
             </div>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
