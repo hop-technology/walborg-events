84a9328e0678f98ed9a31239f955761932f108a8
lukeperrin10 <lukeperrin27@gmail.com>
2022-03-21 15:22:27 +0100

    sets up venue information test and section

---
+++ components/VenueInfo.jsx
@@ -0,0 +1,13 @@
+export default function Venue() {
+  return (
+  <div>
+    <h1>Magasinsgatan 1</h1>
+    <p>
+      Välkommen till en magisk oas i centrala Göteborg! Här arrangerar vi allt
+      från utbildningar, konferenser till stora fester. Magasinsgatan 1 rymmer
+      upp till 200 personer och är den perfekta platsen för att skapa
+      oförglömliga möten och stärka relationer.
+    </p>
+  </div>
+  );
+}
