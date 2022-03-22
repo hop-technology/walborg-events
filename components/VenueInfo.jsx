a1bb5e6ec65d38a8cf76437a0972ea24e1fe243c
lukeperrin10 <lukeperrin27@gmail.com>
2022-03-22 13:25:13 +0100

    fixes formatting issue

--- components/VenueInfo.jsx
+++ components/VenueInfo.jsx
@@ -4,12 +4,12 @@ import { VenueTree } from "./svgs/VenueTree";
 const Venue = () => {
   return (
     <>
-      <div className="venue">
-        <div className="venue__info__title">
-          <h1 data-cy="venue-title">Magasinsgatan 1</h1>
+      <div className='venue'>
+        <div className='venue__info__title'>
+          <h1 data-cy='venue-title'>Magasinsgatan 1</h1>
         </div>
-        <div className="venue__info__description">
-          <p data-cy="venue-info">
+        <div className='venue__info__description'>
+          <p data-cy='venue-info'>
             Välkommen till en magisk oas i centrala Göteborg! Här arrangerar vi
             allt från utbildningar, konferenser till stora fester. Magasinsgatan
             1 rymmer upp till 200 personer och är den perfekta platsen för att
@@ -17,15 +17,15 @@ const Venue = () => {
           </p>
         </div>
         <div>
-          <Link href="#contact" scroll>
-            <button className="btn__submit">
-              <div className="btn__submit--border">
-                <p className="btn__submit--text">Boka nu</p>
+          <Link href='#contact' scroll>
+            <button className='btn__submit'>
+              <div className='btn__submit--border'>
+                <p className='btn__submit--text'>Boka nu</p>
               </div>
             </button>
           </Link>
         </div>
-        <div className="venue__tree">
+        <div className='venue__tree'>
           <VenueTree />
         </div>
       </div>
