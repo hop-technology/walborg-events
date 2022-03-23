f29f6f81fef60602681f0b9ce366da10c6280211
kim_haaga <kim.haaga@live.se>
2022-03-23 20:02:52 +0100

    Add responsive ness to contact-info-section

--- components/ContactInfo.jsx
+++ components/ContactInfo.jsx
@@ -1,37 +1,49 @@
-import Link from 'next/link';
-import Image from 'next/image';
-
+import Link from 'next/link'
+import Image from 'next/image'
 
 const ContactInfo = () => {
   return (
     <>
-    <div className='contact_info'>
-      <div className='contact_info__container'>
-        <div className='contact_info__text'>
-          <p>
-            För priser och tillgänglighet vänligen kontakta oss nedan eller{' '}
-            <br></br>skriv direkt till: <a href='mailto: event@walborgventures.com'>event@walborgventures.com</a>
-          </p>
+      <div className='contact_info'>
+        <div className='contact_info__container'>
+          <div className='contact_info__text'>
+            <p>
+              För priser och tillgänglighet vänligen kontakta oss nedan eller{' '}
+              <br></br>skriv direkt till:{' '}
+              <a href='mailto: event@walborgventures.com'>
+                event@walborgventures.com
+              </a>
+            </p>
+          </div>
+          <div className='contact_info__btn'>
+            <Link href='#contact' passHref scroll>
+              <button className='btn__submit'>
+                <div className='btn__submit--border'>
+                  <p className='btn__submit--text'>Boka nu</p>
+                </div>
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className='contact_info__btn'>
-          <Link href='#contact' passHref scroll>
-            <button className='btn__submit'>
-              <div className='btn__submit--border'>
-                <p className='btn__submit--text'>Boka nu</p>
-              </div>
-            </button>
-          </Link>
+        <div className='contact_info__image'>
+          <Image
+            src='/madhatters.png'
+            alt='Walborg event'
+            height={600}
+            width={600}
+          />
+        </div>
+        <div className='contact_info__tree'>
+          <Image
+            src='/white-tree.svg'
+            alt='Walborg event'
+            height={1030}
+            width={1019}
+          />
         </div>
       </div>
-      <div className='contact_info__image'>
-        <Image src='/madhatters.png' alt='Walborg event' height={500} width={500} />
-      </div>
-      <div className='contact_info__tree'>
-        <Image src='/white-tree.svg' alt='Walborg event' height={1030} width={1019} />
-      </div>
-    </div>
     </>
-  );
-};
+  )
+}
 
-export default ContactInfo;
+export default ContactInfo
