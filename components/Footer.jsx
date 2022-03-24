574a8f7195a3b3c094c2bbaf4fa2c4c797a92656
kim_haaga <kim.haaga@live.se>
2022-03-24 12:18:38 +0100

    Make footer contact info responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,17 +4,17 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer__contact'>
-        <p className='footer__contact--phone'>Telefon: 073-600 44 68</p>
-        <p className='footer__contact--email'>
-          Email: Event@WalborgVentures.com
-        </p>
+        <p>Telefon: 073-600 44 68</p>
+        <p>Email: Event@WalborgVentures.com</p>
       </div>
       <div className='footer__shape'>
         <div className='footer__shape--desktop'>
-          <Image src="/desk-footer.svg"
-          width="3840px"
-          height="1350px"
-          alt="footer" />
+          <Image
+            src='/desk-footer.svg'
+            width='3840px'
+            height='1350px'
+            alt='footer'
+          />
         </div>
         <div className='footer__shape--walborg'>
           <Image
@@ -28,24 +28,33 @@ const Footer = () => {
           walborg kan existera tack vare partnerskap med
         </p>
         <div className='footer__partners'>
-          <div className='footer__partners--logo'> <Image
-            src='/hop-logo.svg'
-            height='300px'
-            width='300px'
-            alt='hop technology logo'
-          /></div>
-          <div className='footer__partners--logo'> <Image
-            src='/wallenstam-logo.svg'
-            height='300px'
-            width='300px'
-            alt='wallenstam logo'
-          /></div>
-          <div className='footer__partners--logo'> <Image
-            src='/colorich-logo.svg'
-            height='300px'
-            width='300px'
-            alt='colorich holding logo'
-          /></div>
+          <div className='footer__partners--logo'>
+            {' '}
+            <Image
+              src='/hop-logo.svg'
+              height='300px'
+              width='300px'
+              alt='hop technology logo'
+            />
+          </div>
+          <div className='footer__partners--logo'>
+            {' '}
+            <Image
+              src='/wallenstam-logo.svg'
+              height='300px'
+              width='300px'
+              alt='wallenstam logo'
+            />
+          </div>
+          <div className='footer__partners--logo'>
+            {' '}
+            <Image
+              src='/colorich-logo.svg'
+              height='300px'
+              width='300px'
+              alt='colorich holding logo'
+            />
+          </div>
         </div>
       </div>
     </div>
