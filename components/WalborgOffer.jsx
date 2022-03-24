52593c8820e140cb40649412dc01d5dee38d3d28
lukeperrin10 <lukeperrin27@gmail.com>
2022-03-24 10:49:40 +0100

    updates styling for offer component

--- components/WalborgOffer.jsx
+++ components/WalborgOffer.jsx
@@ -7,26 +7,27 @@ const WalborgOffer = () => {
         <div className='walborg_offer__container'>
           <div className='walborg_offer__image'>
             <Image
-              src='/madhatters.png'
+              src='/pinkguy.png'
               alt='Walborg event'
               height={600}
               width={600}
             />
           </div>
-
-          <div className='walborg_offer__text'>
-            <p>
-              Walborg kan skräddarsy ditt evenemang efter dina önskemål. Vi
-              levererar förtärning, teknik, ljud, ljus, scen och möblering för
-              upp till 200 personer.
-            </p>
-            <br></br>
-            <p>
-              Magasinsgatan 1 har en unik karaktär med fantastisk ljus. Lokalen
-              har en skön orangeri-känsla med glastaket som släpper in
-              dagsljuset och ger en härlig rymd. Priset för lokalen startar på
-              4000 sek.
-            </p>
+          <div className='walborg_offer__text_container'>
+            <div className='walborg_offer__text'>
+              <p>
+                Walborg kan skräddarsy ditt evenemang efter dina önskemål. Vi
+                levererar förtärning, teknik, ljud, ljus, scen och möblering för
+                upp till 200 personer.
+              </p>
+              <br></br>
+              <p>
+                Magasinsgatan 1 har en unik karaktär med fantastisk ljus.
+                Lokalen har en skön orangeri-känsla med glastaket som släpper in
+                dagsljuset och ger en härlig rymd. Priset för lokalen startar på
+                4000 sek.
+              </p>
+            </div>
           </div>
         </div>
         {/*    <div className='walborg_offer__tree'>
