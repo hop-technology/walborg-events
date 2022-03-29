b1a2d814695100a4075fca7b7c30287eaacc3704
kim_haaga <kim.haaga@live.se>
2022-03-29 15:57:23 +0200

    remove src from twitter img meta

--- components/SEO/socialTags.js
+++ components/SEO/socialTags.js
@@ -28,7 +28,7 @@ const socialTags = ({
         settings.meta.social &&
         settings.meta.social.twitter,
     },
-    { name: "twitter:image:src", content: image },
+    { name: "twitter:image", content: image },
     { name: "twitter:card", content: "summary_large_image" },
     { name: "og:title", content: title },
     { name: "og:type", content: openGraphType },
