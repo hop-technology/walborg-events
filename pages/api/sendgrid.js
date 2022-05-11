7f46cd103253f260a034bee260840b44a925e591
kim_haaga <kim.haaga@live.se>
2022-05-11 13:25:17 +0200

    update mail interface

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -7,34 +7,31 @@ async function sendEmail(req, res) {
     await sendgrid.send({
       to: 'kim.haaga@hoptech.se', // Your email where you'll receive emails
       from: 'kim.haaga@live.se', // your website email address here
-      subject: `Testing 1`,
+      subject: `Walborg Events Form`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
-      <head>
-        <meta charset="utf-8">
-      
-        <title>The HTML5 Herald</title>
-        <meta name="description" content="The HTML5 Herald">
-        <meta name="author" content="SitePoint">
-      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-      
-        <link rel="stylesheet" href="css/styles.css?v=1.0">
-      
-      </head>
-      
-      <body>
-        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
-              </div>
-              <div class="container" style="margin-left: 20px;margin-right: 20px;">
-                <h3>You've got a new mail from ${req.body.fullname}, their email is: ✉️${req.body.email}, their Phone is ${req.body.phone} </h3>
-                  <div style="font-size: 16px;">
-                    <p>Message:</p>
-                    <p>${req.body.message}</p>
-                    <br>
-                  </div>
-                <p class="footer" style="font-size: 16px;padding-bottom: 20px;border-bottom: 1px solid #D1D5DB;">Regards<br>Manu Arora<br>Software Developer<br>+91 9587738861</p>
-              </div>
-      </body>
+        <head>
+          <meta charset="utf-8">
+          <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+        </head>
+        <body>
+          <div class="container" style="margin-left: 20px;margin-right: 20px;">
+            <h3>You've got a new mail from ${req.body.fullname} who submitted the form on Walborg Event page.</h3>
+            <div>
+            <h2>Their name is:</h2>
+            <h3>${req.body.fullname}</h3>
+            <br>
+            <h2>Their Email is:</h2>
+            <h3>✉️ ${req.body.email}</h3>
+            <br>
+            <h2>Their phone number is:</h2>
+            <h3>📞 ${req.body.phone}</h3>
+            <br>
+            <h2>Message:</h2>
+            <h3>${req.body.message}</h3>
+            <br>
+          </div>
+        </body>
       </html>`,
     })
   } catch (error) {
